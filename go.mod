7d2c455183dd0d4956f4828987952074e24fb56b
utgwkk <utagawakiki@gmail.com>
2023-08-08 20:16:03 +0900

    give options from CLI args

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/utgwkk/go-mysqlping
 
 go 1.18
 
-require github.com/go-sql-driver/mysql v1.7.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jessevdk/go-flags v1.5.0
+)
+
+require golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
--- main.go
+++ main.go
@@ -3,18 +3,47 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/jessevdk/go-flags"
 )
 
+var version string = "(unset)"
+
+type Opts struct {
+	User     string `short:"u" long:"user" description:"MySQL user name" value-name:"user_name" default:"root"`
+	Password string `short:"p" long:"password" description:"MySQL user password" value-name:"password"`
+	Host     string `short:"h" long:"host" description:"Host to connect to the MySQL server" value-name:"host_name" default:"127.0.0.1"`
+	Port     uint   `short:"P" long:"port" description:"Port used for the connection" value-name:"port_num" default:"3306"`
+	Help     bool   `long:"help" description:"Show this help"`
+	Version  bool   `long:"version" description:"Show this version"`
+}
+
 func main() {
+	var opts Opts
+	parser := flags.NewParser(&opts, flags.None)
+	if _, err := parser.Parse(); err != nil {
+		log.Fatal(err)
+	}
+
+	if opts.Help {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(0)
+	}
+
+	if opts.Version {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
-	dsn := buildDSN()
+	dsn := buildDSN(opts)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
@@ -26,11 +55,10 @@ func main() {
 	log.Println("Ping successful")
 }
 
-func buildDSN() string {
+func buildDSN(opts Opts) string {
 	cfg := mysql.NewConfig()
-	// TODO: give options from CLI args
-	cfg.Addr = "127.0.0.1:3306"
-	cfg.User = "root"
-	cfg.Passwd = "root"
+	cfg.Addr = fmt.Sprintf("%s:%d", opts.Host, opts.Port)
+	cfg.User = opts.User
+	cfg.Passwd = opts.Password
 	return cfg.FormatDSN()
 }
